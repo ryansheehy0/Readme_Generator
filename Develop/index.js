3a6bbdd51e437ce71efca013ad917a0270b1b789
Ryan Sheehy <ryansheehy0@gmail.com>
2023-08-05 19:18:33 -0700

    Adds questions to get info for readme.

---
+++ Develop/index.js
@@ -0,0 +1,39 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  "What is the title of your project?",
+  // Description
+    "What was your motivation for creating this project?",
+    "What problem does your project solve and why did you build it?",
+    "What did you learn from this project?",
+  // Installation
+    "What are the steps required to install your project?",
+  // Usage
+    "How do you use your project?",
+    "How many screenshot do you want to include?",
+  // Credits
+    "How many people did you collaborated with?",
+    // Person 1
+      "What is their name?",
+      "What is the link to their github profile?",
+    "How many resources did you use?",
+    // Resource 1
+      "What is the name of this resource?",
+      "What is the link to this resource?",
+  // Issues
+    "How can you report an issue?",
+  // Contributing
+    "How can someone else contribute to this project?",
+  // License
+    "What license is your project under?"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
---
+++ README.md
@@ -0,0 +1,14 @@
+# Title
+
+## Description
+
+## Installation
+
+## Usage
+
+## Credits
+
+## Reporting Issues
+how to report issues
+
+## How to Contribute
